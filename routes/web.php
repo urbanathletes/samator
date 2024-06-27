cdb434d9c7784dbf5ae7089d3b2759b5e75fae64
Urban Athletes <itprogrammer@urbanathletes.co.id>
2024-06-27 13:35:10 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -7,21 +7,23 @@ use App\Http\Controllers\RemoveAccountController;
 use App\Http\Controllers\Promo99KController;
 use App\Http\Controllers\RefferalController;
 
-Route::resource('/', AdsController::class);
-//special deal
-Route::resource('/special-deal', SpecialController::class);
-//remove account
-Route::resource('/remove-account', RemoveAccountController::class);
-//refferal
-Route::controller(RefferalController::class)->group(function () {
-    Route::get('/refferal/question/{id}', 'question');
-    Route::post('/refferal/save-guest', 'saveGuest');
-    Route::post('/refferal/unlock-membership', 'unlockMembership');
-    Route::post('/refferal/choose-package', 'choosePackage');
-    Route::post('/refferal/order', 'order');
-});
-Route::resource('/refferal', RefferalController::class);
+// Route::resource('/', AdsController::class);
+Route::resource('/', Promo99KController::class);
 
-//promo 99k
+//special deal
+// Route::resource('/special-deal', SpecialController::class);
+//remove account
+// Route::resource('/remove-account', RemoveAccountController::class);
+//refferal
+// Route::controller(RefferalController::class)->group(function () {
+//     Route::get('/refferal/question/{id}', 'question');
+//     Route::post('/refferal/save-guest', 'saveGuest');
+//     Route::post('/refferal/unlock-membership', 'unlockMembership');
+//     Route::post('/refferal/choose-package', 'choosePackage');
+//     Route::post('/refferal/order', 'order');
+// });
+// Route::resource('/refferal', RefferalController::class);
+
+//Samator
 Route::resource('/samator', Promo99KController::class);
 Route::post('/order', [Promo99KController::class, 'order']);
